1c59633963b40e0a4122e90ffb8f35aa2c40bb6e
Ilya <sange0337@gmail.com>
2024-08-12 18:44:22 +0800

    added the CSP policy

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,6 @@ function redirectAfterDelay() {
     }, 3000);
 }
 
-function redirectToGoogle() {
-    window.location.href = "https://www.google.com";
-}
-
 document.addEventListener('keydown', function(event) {
     if (keys.includes(event.key)) {
         event.preventDefault(); 
